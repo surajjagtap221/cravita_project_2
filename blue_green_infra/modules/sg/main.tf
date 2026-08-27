5745684c7687a0a44d1eec81ef76cfa361bd5b29
Surajjagtap221 <srj412305@gmail.com>
2026-08-27 17:46:13 +0530

    updating terraform code adding SG-Code

--- blue_green_infra/modules/sg/main.tf
+++ blue_green_infra/modules/sg/main.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "blue_green_instance_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
